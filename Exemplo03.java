7c443896fd84cd5ff5651c364ea0ed1b14b29c4a
CrispimPaiano <crispimpds5@gmail.com>
2018-06-05 11:31:06 -0300

    plus: Exercicio03

---
+++ Exemplo03.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemploarraylist;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Exemplo03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            numeros.add(Integer.parseInt(JOptionPane.showInputDialog("Digite um número")));
+        }
+        
+    }
+    
+}
